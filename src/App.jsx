a5ce635b6de65f56e0de4fe7663645c9606ec0a5
shraaavss <shravs2707@gmail.com>
2023-08-15 16:40:15 +0530

    pole done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react"
+import Tiranga from "./Tiranga"
 
-function App() {
-
-  return (
-    <>
-      
-    </>
+export default function App(){
+  return(
+    <div>
+      <Tiranga />
+    </div>
   )
 }
-
-export default App
---
+++ src/Tiranga.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Tiranga.css'
+
+const Tiranga = () => {
+  return (
+    <div className='flag'>
+
+        <div className="flag-pole">
+
+        </div>
+
+        <div className="flag-tricolor">
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Tiranga
