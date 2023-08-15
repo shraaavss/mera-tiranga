df4e6ee5025d5936b15fe3258440aa95cde928d6
shraaavss <shravs2707@gmail.com>
2023-08-15 17:50:01 +0530

    Flag done

--- src/Tiranga.jsx
+++ src/Tiranga.jsx
@@ -2,19 +2,64 @@ import React from 'react'
 import './Tiranga.css'
 
 const Tiranga = () => {
-  return (
-    <div className='flag'>
+    return (
+        <div className="wrapper">
 
-        <div className="flag-pole">
+            <div className='flag'>
+
+                <div className="flag-pole">
+
+                </div>
+
+                <div className="flag-tricolor">
+
+                    <div className="orang">
+
+                    </div>
+
+                    <div className="white">
+
+                        <div className="ashoka-chakra">
+
+                            <div className="spoke spoke-1"></div>
+                            <div className="spoke spoke-2"></div>
+                            <div className="spoke spoke-3"></div>
+                            <div className="spoke spoke-4"></div>
+                            <div className="spoke spoke-5"></div>
+                            <div className="spoke spoke-6"></div>
+                            <div className="spoke spoke-7"></div>
+                            <div className="spoke spoke-8"></div>
+                            <div className="spoke spoke-9"></div>
+                            <div className="spoke spoke-10"></div>
+                            <div className="spoke spoke-11"></div>
+                            <div className="spoke spoke-12"></div>
+                            <div className="spoke spoke-13"></div>
+                            <div className="spoke spoke-14"></div>
+                            <div className="spoke spoke-15"></div>
+                            <div className="spoke spoke-16"></div>
+                            <div className="spoke spoke-17"></div>
+                            <div className="spoke spoke-18"></div>
+                            <div className="spoke spoke-19"></div>
+                            <div className="spoke spoke-20"></div>
+                            <div className="spoke spoke-21"></div>
+                            <div className="spoke spoke-22"></div>
+                            <div className="spoke spoke-23"></div>
+                            <div className="spoke spoke-24"></div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="green">
+
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
-
-        <div className="flag-tricolor">
-
-        </div>
-
-    </div>
-  )
+    )
 }
 
 export default Tiranga
